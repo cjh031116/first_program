f2a95da3b4707521e21deb5d1a0c56fe45ae814a
cjh031116 <936845421@qq.com>
2025-11-17 21:40:02 +0800

    test2

--- app/src/main/java/com/example/recyclerviewtest/FruitAdapter.kt
+++ app/src/main/java/com/example/recyclerviewtest/FruitAdapter.kt
@@ -77,7 +77,7 @@ class FruitAdapter(private val fruitList: List<Fruit>) :
         val viewHolder = ViewHolder(view)
 
         // 为整个列表项设置点击事件
-        viewHolder.itemView.setOnClickListner {
+        viewHolder.itemView.setOnClickListener {
             val position = viewHolder.adapterPosition // 获取点击项的位置
             val fruit = fruitList[position]
             Toast.makeText(parent.context, "你点击了: ${fruit.name}", Toast.LENGTH_SHORT).show()
